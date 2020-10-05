5707a764bb8ecc98c8664fe6c9969ffa5b7e09ec
Andrey <andy.paderin@gmail.com>
2020-10-05 18:35:48 +0300

    Add OAuth

---
+++ lib/true-conf/client.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require 'oauth2'
+require 'pry'
+
+module TrueConf
+  class Client < Evil::Client
+    option :client_id, proc(&:to_s)
+    option :client_secret, proc(&:to_s)
+    option :api_server, proc(&:to_s)
+    option :token_url, proc(&:to_s), default: -> { '/oauth2/v1/token' }
+    option :version, proc(&:to_s), default: -> { '3.1' }
+
+    format 'json'
+    path { "https://#{api_server}/api/v#{version}" }
+
+    class Resolver::Security < Resolver
+      def access_token(client_id, client_secret)
+        client = OAuth2::Client.new(
+          client_id,
+          client_secret,
+          site: "https://#{api_server}",
+          token_url: token_url
+        )
+        client.client_credentials.get_token.token
+      end
+    end
+
+    security do
+      token_auth(access_token(client_id, client_secret), inside: :query)
+    end
+
+    operation :conferences do
+      http_method :get
+      path { '/conferences' }
+
+      response(200) do |aaa, bbb, ccc|
+        binding.pry
+        # |*res| Entity::Payment.build(*res) }
+      end
+
+      response(400, 404) do
+        binding.pry
+        # |*res| Error.build(*res)
+      end
+    end
+  end
+end
--- true-conf.gemspec
+++ true-conf.gemspec
@@ -21,6 +21,7 @@ Gem::Specification.new do |spec|
   spec.required_ruby_version = '>= 2.5'
 
   spec.add_runtime_dependency 'evil-client', '~> 3.0'
+  spec.add_runtime_dependency 'oauth2', '~> 1.4'
 
   spec.add_development_dependency 'bundler', '~> 1.17'
   spec.add_development_dependency 'rake', '~> 13.0'
