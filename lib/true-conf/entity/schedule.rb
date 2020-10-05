0b6ccc2e7cb48db298bbd7e1a3a5e6b305fab0ed
Andrey <andy.paderin@gmail.com>
2020-10-05 22:41:16 +0300

    Add Schedule

---
+++ lib/true-conf/entity/schedule.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module TrueConf
+  module Entity
+    class Schedule
+      extend  Dry::Initializer
+      extend  TrueConf::Callable
+      include TrueConf::Optional
+
+      SCHEDULE_TYPE = {
+        none: -1,
+        weekly: 0,
+        one_time: 1
+      }.freeze
+
+      option :type, proc(&:to_i)
+      option :start_time, proc(&:to_i), optional: true
+      option :duration, proc(&:to_i), optional: true
+
+      option :days, [proc(&:to_s)], optional: true
+      option :time, proc(&:to_s), optional: true
+      option :time_offset, proc(&:to_i), optional: true
+
+      SCHEDULE_TYPE.each do |method_name, value|
+        define_method("#{method_name}?") do
+          type == value
+        end
+      end
+    end
+  end
+end
