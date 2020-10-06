8972a0a522d9e1ee09d03ec6a44c9b25e4912cc0
Andrey <andy.paderin@gmail.com>
2020-10-07 00:26:39 +0300

    Add Simple Conference

---
+++ lib/true-conf/entity/conference_simple.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module TrueConf
+  module Entity
+    class ConferenceSimple < TrueConf::Response
+      option :id, proc(&:to_s), optional: true
+      option :owner, proc(&:to_s), optional: true
+      option :state, proc(&:to_s), optional: true
+
+      class << self
+        def build(*res)
+          body = res.last
+          new JSON.parse(body.first)
+        end
+      end
+    end
+  end
+end
